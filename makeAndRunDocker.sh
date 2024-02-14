a887a3946be175a9af8e69bb72ca7d54506ed95f
Thiago Moura <thiago.moura@ccc.ufcg.edu.br>
2024-02-13 23:54:53 -0300

    add docker files of utilization repository

--- makeAndRunDocker.sh
+++ makeAndRunDocker.sh
@@ -18,6 +18,6 @@ docker build -t testar-local/testar:latest .
 docker run --rm  --net=host --shm-size=512m --name testar \
 --mount type=bind,source="/home/lsi/qa/testar-utilization/TESTAR_dev",target=/mnt \
 --mount type=bind,source="/home/lsi/qa/testar-utilization/TESTAR_dev/runImage",target=/runImage \
---mount type=bind,source="/home/lsi/qa/testar-utilization/testar_docker/output",target=/testar/bin/output \
---mount type=bind,source="/home/lsi/qa/testar-utilization/testar_docker/settings",target=/testar/bin/settings \
+--mount type=bind,source="/home/lsi/qa/testar-utilization/docker/output",target=/testar/bin/output \
+--mount type=bind,source="/home/lsi/qa/testar-utilization/docker/settings",target=/testar/bin/settings \
 testar-local/testar:latest
